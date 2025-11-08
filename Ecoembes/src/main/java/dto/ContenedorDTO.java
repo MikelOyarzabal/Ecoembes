61993c69ebb49cadcdc211023988ca45e4fdce93
Gorka Villalba <145966880+GorkaVillalba@users.noreply.github.com>
2025-11-08 18:22:35 +0100

    Add DTO and entity classes for Camion, Contenedor, Ruta
    Introduces CamionDTO, ContenedorDTO, and RutaDTO classes for data transfer, along with Camion, Contenedor, Llenado, and Ruta entity classes. Adds fields, constructors, getters, and setters to Contenedor and Ruta classes, and updates ContenedorDTO to include relevant properties and methods.

--- Ecoembes/src/main/java/dto/ContenedorDTO.java
+++ Ecoembes/src/main/java/dto/ContenedorDTO.java
@@ -1,5 +1,47 @@
 package dto;
 
-public class ContenedorDTO {
+import entity.Llenado;
 
+public class ContenedorDTO {
+	private long id;
+	private String ubicacion;
+	private float capacidad;
+	private Llenado nivelDeLlenado;
+	
+	public ContenedorDTO(long id, String ubicacion, float capacidad, Llenado nivelDeLlenado) {
+		super();
+		this.id = id;
+		this.ubicacion = ubicacion;
+		this.capacidad = capacidad;
+		this.nivelDeLlenado = nivelDeLlenado;
+	}
+	
+	public ContenedorDTO() {
+		super();
+	}
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	public float getCapacidad() {
+		return capacidad;
+	}
+	public void setCapacidad(float capacidad) {
+		this.capacidad = capacidad;
+	}
+	public Llenado getNivelDeLlenado() {
+		return nivelDeLlenado;
+	}
+	public void setNivelDeLlenado(Llenado nivelDeLlenado) {
+		this.nivelDeLlenado = nivelDeLlenado;
+	}
 }
--- Ecoembes/src/main/java/entity/Contenedor.java
+++ Ecoembes/src/main/java/entity/Contenedor.java
@@ -1,5 +1,50 @@
 package entity;
 
 public class Contenedor {
+	private long id;
+	private String ubicacion;
+	private float capacidad;
+	private Llenado nivelDeLlenado;
+	
+	public Contenedor(long id, String ubicacion, float capacidad, Llenado nivelDeLlenado) {
+		super();
+		this.id = id;
+		this.ubicacion = ubicacion;
+		this.capacidad = capacidad;
+		this.nivelDeLlenado = nivelDeLlenado;
+	}
+	
+	public Contenedor() {
+		super();
+	}
+	public Llenado getEstadoContenedor() {
+		return this.nivelDeLlenado;
+		
+	}
 
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	public float getCapacidad() {
+		return capacidad;
+	}
+	public void setCapacidad(float capacidad) {
+		this.capacidad = capacidad;
+	}
+	public Llenado getNivelDeLlenado() {
+		return nivelDeLlenado;
+	}
+	public void setNivelDeLlenado(Llenado nivelDeLlenado) {
+		this.nivelDeLlenado = nivelDeLlenado;
+	}
+	
 }
